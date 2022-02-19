583716c0ff777d25a9739a487a9781176d930ace
trimed <mohamed.tribak@gmail.com>
2022-02-19 22:56:42 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Tribak 2022  ba3d meni!");
+        System.out.println("Hello, Tribak 2022  Sma7li a khokho!");
     }
 }
